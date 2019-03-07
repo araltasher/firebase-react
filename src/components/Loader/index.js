d702619a5740da3479c90179d3457f8560cce002
Aral Tasher <araltasher@gmail.com>
2019-03-07 00:33:13 -0500

    refactored the loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './loader.css';
 
-const Loader = () => <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+const Loader = () => <div className="lds-grid">
+        <div></div><div></div><div></div>
+        <div></div><div></div><div></div>
+        <div></div><div></div><div></div>
+    </div>
 
 export default Loader;
