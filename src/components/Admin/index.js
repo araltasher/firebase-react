202dc8a61de4d2ac743fb668482ebae265835ad3
Aral Tasher <araltasher@gmail.com>
2019-03-13 16:30:21 -0400

    user detail view, rearranging the admin structure

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { compose } from 'recompose';
 import { withFirebase } from '../Firebase';
 import { withAuthorization, withEmailVerification } from '../Session';
-import * as ROLES from '../../constants/roles';
 import Loader from '../Loader';
+import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-class Admin extends Component {
+const Admin = () => (
+  <div>
+    <h1>Admin</h1>
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   constructor(props) {
     super(props);
 
@@ -39,50 +51,113 @@ class Admin extends Component {
 
   render() {
     const { users, loading } = this.state;
-    const center = {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
+    const listStyle = {
+      marginBottom: '2rem',
     };
+
     return (
       <div>
-        <h1>Admin</h1>
+        <h2>
+          Users
+       </h2>
         {loading && (
           <div style={center}>
             <Loader />
           </div>
         )}
-        <UserList users={users} />
+        <ul>
+          {users.map(user => (
+            <li style={listStyle} key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.uid}
+              </span>
+              <br />
+              <span>
+                <strong>E-Mail:</strong> {user.email}
+              </span>
+              <br />
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <br />
+              <span>
+                <Link to={`${ROUTES.ADMIN}/${user.uid}`}>
+                  Details
+                </Link>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const listStyle = {
-  marginBottom: '2rem',
-};
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      user: null,
+    };
+  }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li style={listStyle} key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.uid}
-        </span>
-        <br />
-        <span>
-          <strong>E-Mail:</strong> {user.email}
-        </span>
-        <br />
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on('value', snapshot => {
+        this.setState({
+          user: snapshot.val(),
+          loading: false,
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  render() {
+    const { user, loading } = this.state;
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && (
+          <div style={center}>
+            <Loader />
+          </div>
+        )}
+
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <br />
+            <span>
+              <strong>E-Mail:</strong> {user.email}
+            </span>
+            <br />
+            <span>
+              <strong>Full Name:</strong> {user.username}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 const condition = authUser => authUser && authUser.roles.includes(ROLES.ADMIN);
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
+const center = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 export default compose(
   withEmailVerification,
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { withAuthentication } from '../Session';
+
 import Nav from '../Navigation';
 import LandingPage from '../Landing';
 import SignUp from '../SignUp';
@@ -11,6 +11,7 @@ import Account from '../Account';
 import Admin from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
@@ -18,12 +19,12 @@ const App = () => (
       <Nav />
       <div className="wrapper">
         <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-        <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-        <Route exact path={ROUTES.HOME} component={Home} />
-        <Route exact path={ROUTES.ACCOUNT} component={Account} />
-        <Route exact path={ROUTES.ADMIN} component={Admin} />
+        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route path={ROUTES.ADMIN} component={Admin} />
       </div>
     </>
   </Router>
