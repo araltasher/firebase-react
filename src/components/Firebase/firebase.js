dcf0909590566ce13e49eeaa4f258b893b6f9e21
Aral Tasher <araltasher@gmail.com>
2019-03-18 17:14:29 -0400

    deleteing chat implemented

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -81,8 +81,11 @@ class Firebase {
 
   //    *** USER API ***    //
   user = uid => this.db.ref(`users/${uid}`);
-
   users = () => this.db.ref('users');
+
+  //    *** PORTFOLIO API ***    //
+message = uid => this.db.ref(`messages/${uid}`);
+messages = () => this.db.ref('messages');
 }
 
 export default Firebase;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,135 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
+import { withFirebase } from '../Firebase';
+import Loader from '../Loader';
 
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <p> The homepage is accesible by signed in users only</p>
+    <Messages />
   </div>
 );
 
 const condition = authUser => !!authUser;
+
+class MessagesBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      loading: false,
+      messages: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.messages().on('value', snapshot => {
+      const messageObject = snapshot.val();
+      if (messageObject) {
+        const messageList = Object.keys(messageObject).map(key => ({
+          ...messageObject[key],
+          uid: key,
+        }));
+
+        this.setState({
+          messages: messageList,
+          loading: false
+        });
+      } else {
+        this.setState({ messages: null, loading: false });
+      }
+    });
+  }
+  componentWillMount() {
+    this.props.firebase.messages().off();
+  }
+
+  onChangeText = e => {
+    this.setState({ text: e.target.value });
+  }
+
+  onCreateMessage = (e, authUser) => {
+    this.props.firebase.messages().push({
+      text: this.state.text,
+      userId: authUser.uid
+    });
+    this.setState({ text: '' });
+    e.prevenDefault();
+  }
+
+  onRemoveMessage = uid => {
+    this.props.firebase.message(uid).remove();
+  };
+
+  render() {
+
+    const { text, messages, loading } = this.state;
+
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+
+          <div>
+            {loading && (
+              <div style={center}>
+                <Loader />
+              </div>
+            )}
+            {messages ? (
+              <MessageList 
+                messages={messages}
+                onRemoveMessage={this.onRemoveMessage}
+              />
+            ) : (
+                <div>There are no messages</div>
+              )}
+
+            <form onSubmit={e => this.onCreateMessage(e, authUser)}>
+              <input
+                type="text"
+                value={text}
+                onChange={this.onChangeText}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+    )
+  }
+}
+
+const MessageList = ({ messages, onRemoveMessage }) => (
+  <ul>
+    {messages.map(message => (
+      <MessageItem
+        key={message.uid}
+        message={message}
+        onRemoveMessage={onRemoveMessage}
+      />
+    ))}
+  </ul>
+);
+
+const MessageItem = ({ message, onRemoveMessage }) => (
+  <li>
+    <strong>{message.userId} </strong><br/>{message.text}
+    <button 
+      type="button"
+      onClick={() => onRemoveMessage(message.uid)}>Delete</button>
+  </li>
+);
+
+const Messages = withFirebase(MessagesBase);
+const center = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 export default compose(
   withEmailVerification,
